a967fefad5b7eb8a894fc39c05427c04b2528834
slifland <sethlifland11@gmail.com>
2024-10-09 18:39:30 -0400

    decide_action function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 from pettingzoo.classic import texas_holdem_v4
 
 
+def decide_action(environment):
+    #always fold
+    return 2
+
 def main():
     env = texas_holdem_v4.env(num_players=4, render_mode="human", screen_height = 500)
     env.reset(seed=42)
@@ -14,8 +18,8 @@ def main():
             action = None
         else:
             mask = observation["action_mask"]
-            action = env.action_space(agent).sample(mask)
-            action = 2
+            #action = env.action_space(agent).sample(mask)
+            action = decide_action(env)
             print(action)
 
         env.step(action)
